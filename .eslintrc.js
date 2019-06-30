1e2ffa4b66d8a5ddbe8fae0429d636f576c7f614
React Native Guru <john@reactnative.guru>
2019-06-30 10:58:21 +0100

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'prettier'
+
+  ],
+  'rules': {
+    'no-undef': 'off',
+    'import/no-named-as-default-member': 'off',
+    'react/prefer-stateless-function': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off'
+  },
+}
